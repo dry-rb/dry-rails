a915b319ef1b769d5eb50c7241e71a3b3bf21bfa
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-08-17 01:42:42 +0200

    Infer container automatically based on Rails.root

--- lib/dry/system/rails.rb
+++ lib/dry/system/rails.rb
@@ -4,6 +4,26 @@ require 'rails/railtie'
 module Dry
   module System
     module Rails
+      extend Dry::Configurable
+
+      setting :namespace
+
+      def self.configure
+        return if config.namespace
+        super
+        config.namespace.const_set(:Container, create_container(config.namespace))
+      end
+
+      def self.create_container(namespace)
+        container = Class.new(Dry::System::Container).configure do |config|
+          config.root = ::Rails.root
+          config.system_dir = config.root.join('config/system')
+          config.name = namespace.to_s.underscore.to_sym
+        end
+
+        container.load_paths!('lib', 'app/models')
+      end
+
       class Railtie < ::Rails::Railtie
         config.before_initialize do |app|
         end
--- spec/dummy/config/initializers/container.rb
+++ spec/dummy/config/initializers/container.rb
@@ -1,13 +1,5 @@
 require 'dry/system/rails'
 
-module Dummy
-  class Container < Dry::System::Container
-    configure do |config|
-      config.name = :dummy
-      config.root = Pathname(__dir__).join('../..')
-      config.system_dir = config.root.join('config/system')
-    end
-
-    load_paths!('lib')
-  end
+Dry::System::Rails.configure do |config|
+  config.namespace = Dummy
 end
