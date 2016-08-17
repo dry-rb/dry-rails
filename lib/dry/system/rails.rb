c495da73df7fcdd8c8bb5cf2e8fd49929c37a9e3
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-08-17 02:24:28 +0200

    Infer namespace for container from rails app instance

--- lib/dry/system/rails.rb
+++ lib/dry/system/rails.rb
@@ -6,28 +6,24 @@ module Dry
     module Rails
       extend Dry::Configurable
 
-      setting :namespace
       setting :auto_register, []
 
       def self.configure
-        return if config.namespace
         super
-        container = create_container(config)
-        config.namespace.const_set(:Container, container)
 
-        Railtie.configure do |r|
-          r.config.container = container
+        container = create_container(config)
+
+        Railtie.configure do
+          config.container = container
         end
       end
 
       def self.create_container(defaults)
-        namespace = defaults.namespace
         auto_register = defaults.auto_register
 
         container = Class.new(Dry::System::Container).configure do |config|
           config.root = ::Rails.root
           config.system_dir = config.root.join('config/system')
-          config.name = namespace.to_s.underscore.to_sym
           config.auto_register = auto_register
         end
 
@@ -35,8 +31,26 @@ module Dry
       end
 
       class Railtie < ::Rails::Railtie
-        config.to_prepare do |config|
-          Railtie.config.container.finalize!
+        config.to_prepare do |*args|
+          Railtie.finalize!
+        end
+
+        def finalize!
+          namespace.const_set(:Container, container)
+          container.config.name = name
+          container.finalize!
+        end
+
+        def name
+          namespace.name.underscore.to_sym
+        end
+
+        def namespace
+          Object.const_get(::Rails.application.class.to_s.split('::').first)
+        end
+
+        def container
+          Railtie.config.container
         end
       end
     end
