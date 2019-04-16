c4c443728ccb76a2086f59a4971d97e80560649b
Piotr Solnica <piotr.solnica@gmail.com>
2019-04-16 15:22:52 +0200

    Extract railtie to a separate file / add some yard comments

--- lib/dry/system/rails.rb
+++ lib/dry/system/rails.rb
@@ -1,20 +1,36 @@
 # frozen_string_literal: true
 
-require 'rails/railtie'
+require 'dry/system/rails/railtie'
 require 'dry/system/rails/container'
 
 module Dry
   module System
+    # Initializer interface
+    #
+    # @example set up a customized container
+    #   # config/initializer/system.rb
+    #
+    #   Dry::System::Rails.container do
+    #     use :monitoring
+    #
+    #     config.auto_register << 'app/operations'
+    #   end
+    #
+    # @api public
     module Rails
+      # Set container block that will be evaluated in the context of the container
+      #
+      # @api public
       def self.container(&block)
         @container_block = block
         self
       end
 
-      def self.container_block
-        defined?(@container_block) && @container_block
-      end
-
+      # Create a new container class
+      #
+      # This is used during booting and reloading
+      #
+      # @api private
       def self.create_container(options = {})
         container = Class.new(Container).configure { |config|
           config.root = ::Rails.root
@@ -29,53 +45,9 @@ module Dry
         container
       end
 
-      class Railtie < ::Rails::Railtie
-        config.to_prepare do
-          Railtie.finalize!
-        end
-
-        def finalize!
-          container = System::Rails.create_container(name: name)
-
-          set_or_reload(:Container, container)
-
-          container.refresh_boot_files if reloading?
-
-          container.finalize!(freeze: freeze?)
-
-          set_or_reload(:Import, container.injector)
-        end
-
-        def freeze?
-          !::Rails.env.test?
-        end
-
-        def reloading?
-          app_namespace.const_defined?(:Container)
-        end
-
-        def name
-          app_namespace.name.underscore.to_sym
-        end
-
-        # TODO: we had to rename namespace=>app_namespace because
-        #       Rake::DSL's Kernel#namespace *sometimes* breaks things.
-        #       Currently we are missing specs verifying that rake tasks work
-        #       correctly and those must be added!
-        def app_namespace
-          @app_namespace ||= begin
-            top_level_namespace = ::Rails.application.class.to_s.split('::').first
-            Object.const_get(top_level_namespace)
-          end
-        end
-
-        def set_or_reload(const_name, const)
-          if app_namespace.const_defined?(const_name)
-            app_namespace.__send__(:remove_const, const_name)
-          end
-
-          app_namespace.const_set(const_name, const)
-        end
+      # @api private
+      def self.container_block
+        defined?(@container_block) && @container_block
       end
     end
   end
---
+++ lib/dry/system/rails/railtie.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rails/railtie'
+
+module Dry
+  module System
+    module Rails
+      # System railtie is responsible for setting up a container and handling reloading in dev mode
+      #
+      # @api private
+      class Railtie < ::Rails::Railtie
+        config.to_prepare do
+          Railtie.finalize!
+        end
+
+        # @api private
+        def finalize!
+          container = System::Rails.create_container(name: name)
+
+          set_or_reload(:Container, container)
+
+          container.refresh_boot_files if reloading?
+
+          container.finalize!(freeze: freeze?)
+
+          set_or_reload(:Import, container.injector)
+        end
+
+        # @api private
+        def freeze?
+          !::Rails.env.test?
+        end
+
+        # @api private
+        def reloading?
+          app_namespace.const_defined?(:Container)
+        end
+
+        # @api private
+        def name
+          app_namespace.name.underscore.to_sym
+        end
+
+        # TODO: we had to rename namespace=>app_namespace because
+        #       Rake::DSL's Kernel#namespace *sometimes* breaks things.
+        #       Currently we are missing specs verifying that rake tasks work
+        #       correctly and those must be added!
+        #
+        # @api private
+        def app_namespace
+          @app_namespace ||= begin
+            top_level_namespace = ::Rails.application.class.to_s.split("::").first
+            Object.const_get(top_level_namespace)
+          end
+        end
+
+        # @api private
+        def set_or_reload(const_name, const)
+          if app_namespace.const_defined?(const_name)
+            app_namespace.__send__(:remove_const, const_name)
+          end
+
+          app_namespace.const_set(const_name, const)
+        end
+      end
+    end
+  end
+end
