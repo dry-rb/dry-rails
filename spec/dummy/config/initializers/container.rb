0414073356f3d1dc406b7308c4ac8e68bd085c4b
Tim Riley <tim@openmonkey.com>
2019-09-28 21:45:59 +1000

    Add a test to ensure dry-system plugins (which add settings) can be used

--- spec/dummy/config/initializers/container.rb
+++ spec/dummy/config/initializers/container.rb
@@ -3,6 +3,8 @@
 require 'dry/system/rails'
 
 Dry::System::Rails.container do
+  use :env, inferrer: -> { Rails.env.to_sym }
+
   config.auto_register << 'lib' << 'app/operations'
 
   auto_register!('app/workers') do |config|
