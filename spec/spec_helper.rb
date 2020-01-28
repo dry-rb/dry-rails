67129ce0f00117631352c787334816139b58ca6f
Piotr Solnica <piotr.solnica@gmail.com>
2020-01-28 10:48:56 +0100

    Update coverage setup in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,11 @@
 # frozen_string_literal: true
 
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.safe_load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
-
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
-  end
-end
+require_relative 'support/coverage'
 
 begin
   require 'byebug'
 rescue LoadError; end
+
 require 'dry-rails'
 
 SPEC_ROOT = Pathname(__dir__)
@@ -24,6 +14,7 @@ Dir[SPEC_ROOT.join('shared/**/*.rb')].each(&method(:require))
 Dir[SPEC_ROOT.join('support/**/*.rb')].each(&method(:require))
 
 ENV['RAILS_ENV'] ||= 'test'
+
 require SPEC_ROOT.join('dummy/config/environment')
 
 RSpec.configure do |config|
