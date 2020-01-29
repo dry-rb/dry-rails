ddd4ae6d0c25f96a4e71b355e8f023409bb7f141
Piotr Solnica <piotr.solnica@gmail.com>
2020-01-29 11:10:59 +0100

    Fix flaky spec

--- spec/integration/container/using_plugins_spec.rb
+++ spec/integration/container/using_plugins_spec.rb
@@ -2,6 +2,7 @@
 
 RSpec.describe 'Using dry-system plugins' do
   specify 'with extra settings inside the initializer container block' do
+    Dry::Rails::Railtie.reload
     expect(Dummy::Container.env).to be(Rails.env.to_sym)
   end
 end
