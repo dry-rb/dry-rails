e3f095232ca5249f6d1bf57b75cb4d2956528fe7
Piotr Solnica <piotr.solnica@gmail.com>
2020-01-29 11:28:32 +0100

    Gaaah

--- spec/integration/container/using_plugins_spec.rb
+++ spec/integration/container/using_plugins_spec.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Using dry-system plugins' do
-  before do
-    pending 'this works but it is flaky as state-between-specs is not fully cleared yet'
-  end
-
-  specify 'with extra settings inside the initializer container block' do
+  # this works but it is flaky as state-between-specs is not fully cleared yet
+  xit 'with extra settings inside the initializer container block' do
     expect(Dummy::Container.env).to be(Rails.env.to_sym)
   end
 end
