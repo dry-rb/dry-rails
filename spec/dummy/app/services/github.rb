6a096694786b1cd464364d57d213ae7255e98072
Piotr Solnica <piotr.solnica@gmail.com>
2020-02-15 17:59:33 +0100

    Minor style fixes in specs

--- spec/dummy/app/services/github.rb
+++ spec/dummy/app/services/github.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Dummy
   module Services
     class Github
--- spec/integration/dry/rails/container_spec.rb
+++ spec/integration/dry/rails/container_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Dry::Rails, '.container' do
   subject(:system) { Dummy::Container }
 
