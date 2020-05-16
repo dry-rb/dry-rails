f2bbd153ac0755946dc763e6b01d15c1d41d0edb
Yasha Krasnov <yasha.krasnov@gmail.com>
2020-05-16 16:00:13 +0300

    safe_params are empty in ApplicationController before_action (#26)
    put before action :set_safe_params above other before_actions
    
    closes #25 
    
    Co-authored-by: Piotr Solnica <piotr.solnica@gmail.com>

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   get "users/show/:id" => "users#show"
   get "users/new/:id" => "users#new"
+  
+  get "safe_params_callbacks/show/:id" => "safe_params_callbacks#show"
 end
